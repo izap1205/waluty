a976e5c0fb20d945b62ebe9345871a03555ad068
i.pondo <pondoizabela@gmail.com>
2024-06-09 08:32:30 +0200

    commit

---
+++ frontend/src/app/currency-list/currency-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-list.component.html',
+  styleUrl: './currency-list.component.css'
+})
+export class CurrencyListComponent {
+
+}
